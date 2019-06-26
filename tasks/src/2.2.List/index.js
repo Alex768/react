015029a759bd18fd3b8ad953f158e04ccfc42d94
Alex768 <vortigont768@inbox.ru>
2019-06-26 12:26:59 +0500

    Solve task 2.2

--- tasks/src/2.2.List/index.js
+++ tasks/src/2.2.List/index.js
@@ -5,63 +5,68 @@ import './styles.css';
 /**
      1. Разбери ручные переборы массивов в верстке.
         Для постов используй map без циклов, для авторов цикл for без map.
-    
+
      2. Посмотри ошибки в Chrome DevTools: React должен требовать наличия атрибутов key.
         Добавь в post поле id и присвой каждому полю уникальный строковый идентификатор.
         Используй id в качестве значения key в основном тэге поста и основном тэге автора.
  */
 
 const posts = [
-  {
-    author: 'Парень не промах',
-    time: '2 часа назад',
-    message: 'Попробую с удовольствием ;)'
-  },
-  {
-    author: 'Милая девушка',
-    time: '3 часа назад',
-    message: 'Можно использовать для выпекания чизкейков :)'
-  },
-  {
-    author: 'Скупец',
-    time: 'вчера',
-    message: 'Цену-то загнули!'
-  }
+    {
+        id: 1,
+        author: 'Парень не промах',
+        time: '2 часа назад',
+        message: 'Попробую с удовольствием ;)'
+    },
+    {
+        id: 2,
+        author: 'Милая девушка',
+        time: '3 часа назад',
+        message: 'Можно использовать для выпекания чизкейков :)'
+    },
+    {
+        id: 3,
+        author: 'Скупец',
+        time: 'вчера',
+        message: 'Цену-то загнули!'
+    }
 ];
 
 function renderPost(post) {
-  return (
-    <div className="post">
-      <div className="postHeader">
-        <span className="postAuthor">{post.author}</span>
-        <br />
-        <span className="postTime">{post.time}</span>
-      </div>
-      <div className="postMessage">{post.message}</div>
-    </div>
-  );
+    return (
+        <div className="post" key={'post-'+post.id}>
+            <div className="postHeader">
+                <span className="postAuthor">{post.author}</span>
+                <br/>
+                <span className="postTime">{post.time}</span>
+            </div>
+            <div className="postMessage">{post.message}</div>
+        </div>
+    );
 }
 
 function renderAuthors(posts) {
-  return (
-    <div className="authors">
-      <span>{posts[0].author}</span>
-      <span>{posts[1].author}</span>
-      <span>{posts[2].author}</span>
-    </div>
-  );
+    const authors = [];
+    for (const post of posts)
+    {
+        authors.push(<span key={'author-'+post.id}>{post.author}</span>);
+    }
+    return (
+        <div className="authors">
+            {authors}
+        </div>
+    );
 }
 
+const renderedPosts = posts.map(post => renderPost(post));
 ReactDom.render(
-  <div className="page">
-    <div className="posts">
-      {renderPost(posts[0])}
-      {renderPost(posts[1])}
-      {renderPost(posts[2])}
-    </div>
-    {renderAuthors(posts)}
-  </div>,
-  document.getElementById('app')
+    <div className="page">
+        <div className="posts">
+            {renderedPosts}
+        </div>
+        {renderAuthors(posts)}
+    </div>,
+    document.getElementById('app')
 );
 
 /**
