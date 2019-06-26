7a77f71719671794de70987e0a5017dc4259b95e
Alex768 <vortigont768@inbox.ru>
2019-06-26 11:57:08 +0500

    Solve task 2.1

--- tasks/src/2.1.ExtractFunction/index.js
+++ tasks/src/2.1.ExtractFunction/index.js
@@ -6,34 +6,57 @@ import './styles.css';
      Выдели метод отрисовки лота (renderLot), метод отрисовки поста (renderPost) и используй их.
  */
 
+function renderLot(lot)
+{
+    return (
+        <div className="lot">
+            <div className="lotName">{lot.name}</div>
+            <div className="lotDescription">{lot.description}</div>
+        </div>
+    );
+}
+
+function renderPost(post)
+{
+    return (
+        <div className="post">
+            <div className="postHeader">
+                <span className="postAuthor">{post.author}</span>
+                <br/>
+                <span className="postTime">{post.time}</span>
+            </div>
+            <div className="postMessage">{post.message}</div>
+        </div>
+    );
+}
+
+const posts = [
+    {
+        author: 'Парень не промах',
+        time: '2 часа назад',
+        message: 'Попробую с удовольствием ;)'
+    },
+    {
+        author: 'Милая девушка',
+        time: '3 часа назад',
+        message: 'Можно использовать для выпекания чизкейков :)'
+    }
+];
+
 ReactDom.render(
-  <div className="page">
-    <div className="lot">
-      <div className="lotName">Форма для выпекания</div>
-      <div className="lotDescription">Идеальна для приготовления десертов!</div>
-    </div>
-    <div className="posts">
-      <div className="post">
-        <div className="postHeader">
-          <span className="postAuthor">Парень не промах</span>
-          <br />
-          <span className="postTime">2 часа назад</span>
+    <div className="page">
+        {
+            renderLot({
+                name: 'Форма для выпекания',
+                description: 'Идеальна для приготовления десертов!'
+            })
+        }
+        <div className="posts">
+            {renderPost(posts[0])}
+            {renderPost(posts[1])}
         </div>
-        <div className="postMessage">Попробую с удовольствием ;)</div>
-      </div>
-      <div className="post">
-        <div className="postHeader">
-          <span className="postAuthor">Милая девушка</span>
-          <br />
-          <span className="postTime">3 часа назад</span>
-        </div>
-        <div className="postMessage">
-          Можно использовать для выпекания чизкейков :)
-        </div>
-      </div>
-    </div>
-  </div>,
-  document.getElementById('app')
+    </div>,
+    document.getElementById('app')
 );
 
 /**
